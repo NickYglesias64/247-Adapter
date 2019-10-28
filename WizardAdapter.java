3964086c9b86657c1598af00c7271467a40c755c
thatpaiguy <dhruv.k.pai@gmail.com>
2019-10-27 23:23:31 -0400

    Second commit with comments

--- WizardAdapter.java
+++ WizardAdapter.java
@@ -1,8 +1,10 @@
-public class WizardAdapter implements Warrior {
+public class WizardAdapter implements Warrior { //class gets methods from Warrior class
     private Wizard wizard;
+    //constructor has parameter of wizard using warrior commands
     public WizardAdapter(Wizard w){
         this.wizard = w;
     }
+    //warrior commands that make wizard use wizard commands
     public void attack(){
         wizard.performSpell();
     }
